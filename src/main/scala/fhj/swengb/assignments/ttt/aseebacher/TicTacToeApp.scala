4aee6f54a3f16968413ad116e589e75b18d6c217
Andreas Seebacher <andreas.seebacher@outlook.at>
2015-11-27 17:48:50 +0100

    set uo fxmml and css files

--- src/main/scala/fhj/swengb/assignments/ttt/aseebacher/TicTacToeApp.scala
+++ src/main/scala/fhj/swengb/assignments/ttt/aseebacher/TicTacToeApp.scala
@@ -1,5 +1,55 @@
-package fhj.swengb.assignments.ttt.rladstaetter
+package fhj.swengb.assignments.ttt.aseebacher
 
-/**
-  * Implement here your TicTacToe JavaFX App.
-  */
+import java.net.URL
+import java.util.ResourceBundle
+import javafx.application.Application
+import javafx.fxml.{FXML, FXMLLoader, Initializable}
+import javafx.scene.image.{Image, ImageView}
+import javafx.scene.layout.BorderPane
+import javafx.scene.{Parent, Scene}
+import javafx.stage.Stage
+
+import fhj.swengb.Speakers
+
+import scala.util.control.NonFatal
+
+object TicTacToeApp {
+  def main(args: Array[String]) {
+    Application.launch(classOf[TicTacToeApp], args: _*)
+  }
+}
+
+class TicTacToeApp extends javafx.application.Application {
+
+
+  val Fxml = "/fhj/swengb/assignments/ttt/TicTacToeApp.fxml"
+  val Css = "fhj/swengb/assignments/ttt/TicTacToeApp.css"
+
+  val loader = new FXMLLoader(getClass.getResource(Fxml))
+
+  override def start(stage: Stage): Unit =
+    try {
+      stage.setTitle("Tic Tac Toe")
+      loader.load[Parent]() // side effect
+      val scene = new Scene(loader.getRoot[Parent])
+      stage.setScene(scene)
+      stage.getScene.getStylesheets.add(Css)
+      stage.show()
+    } catch {
+      case NonFatal(e) => e.printStackTrace()
+    }
+
+}
+
+
+class TicTacToeController extends Initializable {
+  @FXML var borderPane: BorderPane = _
+
+  override def initialize(location: URL, resources: ResourceBundle): Unit = {
+    val url: String = "about:blank"
+    //val url = Students.mfuchs.gitHubUser.avatarUrl.toString
+
+    borderPane.setCenter(new ImageView(new Image(url)))
+  }
+
+}
